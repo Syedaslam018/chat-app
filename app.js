9529701ee3cca0f88aa69b9d7c6d57f916870ccd
Syed Aslam <syedaslam.me@gmail.com>
2023-10-13 18:19:53 +0530

    making sure everything is fine before deploying

--- app.js
+++ app.js
@@ -1,49 +1,45 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const bcrypt = require('bcrypt');
-const path = require('path')
-const http = require('http')
-const socketIO = require('socket.io')
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const http = require("http");
+const socketIO = require("socket.io");
+require("dotenv").config();
 
-
-const sequelize = require('./routes/util/database');
+const sequelize = require("./routes/util/database");
 const app = express();
 const server = http.createServer(app);
-const io = socketIO(server, {cors: {origin:'*'}})
+const io = socketIO(server, { cors: { origin: "*" } });
 
-
-io.on('connection', (socket) => {
-    console.log("Socket.io is Conneted");
-    socket.on('message',(msg,userName,groupId,userId)=>{
-        socket.broadcast.emit("message",msg,userName,groupId,userId)
-    });
-    socket.on("file",(message,userName,groupId,userId)=>{
-        socket.broadcast.emit("file",message,userName,userId)
-
-    })
-})
-app.use(cors())
+io.on("connection", (socket) => {
+  console.log("Socket.io is Conneted");
+  socket.on("message", (msg, userName, groupId, userId) => {
+    socket.broadcast.emit("message", msg, userName, groupId, userId);
+  });
+  socket.on("file", (message, userName, groupId, userId) => {
+    socket.broadcast.emit("file", message, userName, userId);
+  });
+});
+app.use(cors());
 app.use(bodyParser.json({ extended: false }));
 
-const User = require('./models/users')
-const Messages = require('./models/messages')
-const Groups = require('./models/groups')
-const UserGroups = require('./models/user-groups')
-const ArchivedChat = require('./models/archivedchat')
+const User = require("./models/users");
+const Messages = require("./models/messages");
+const Groups = require("./models/groups");
+const UserGroups = require("./models/user-groups");
+const ArchivedChat = require("./models/archivedchat");
 
-
-const signupRoutes = require('./routes/user');
-const appRoutes = require('./routes/chatapp');
-const groupRoutes = require('./routes/group')
+const signupRoutes = require("./routes/user");
+const appRoutes = require("./routes/chatapp");
+const groupRoutes = require("./routes/group");
 
 app.use(signupRoutes);
-app.use(appRoutes)
+app.use(appRoutes);
 app.use(groupRoutes);
 app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, `public${req.url}`))
-})
+  res.sendFile(path.join(__dirname, `public${req.url}`));
+});
 
 User.hasMany(Messages);
 Messages.belongsTo(User);
@@ -51,14 +47,14 @@ Messages.belongsTo(User);
 Groups.hasMany(Messages);
 Messages.belongsTo(Groups);
 
-Groups.belongsToMany(User, {through: UserGroups})
-User.belongsToMany(Groups, {through: UserGroups})
+Groups.belongsToMany(User, { through: UserGroups });
+User.belongsToMany(Groups, { through: UserGroups });
 sequelize
-//.sync({force: true})
-.sync()
-.then(user => {
-    server.listen(3000)
-})
-.catch(err => {
-    console.log(err)
-})
+  //.sync({force: true})
+  .sync()
+  .then((user) => {
+    server.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
